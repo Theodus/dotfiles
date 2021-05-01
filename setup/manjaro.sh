5850f7abecb45b087deb25e0cb0ba15cdaf5f128
Theo Butler <theodusbutler@gmail.com>
2021-05-01 12:26:03 -0400

    remove stuff for tiling WMs

--- setup/manjaro.sh
+++ setup/manjaro.sh
@@ -3,39 +3,37 @@
 require() {
   err=false
   for cmd in "$@"; do
-    if ! command -v "$cmd" >/dev/null; then
-      printf "%s %s\n" "$cmd" "command required!"
+    if ! command -v "${cmd}" >/dev/null; then
+      printf "%s %s\n" "${cmd}" "command required!"
       err=true
     fi
   done
-  if [ $err = true ]; then
+  if [ ${err} = true ]; then
     exit 1
   fi
 }
 
 install_tmux_tpm() {
   printf "Installing Tmux Plugin Manager...\n"
-  git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+  git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
 }
 
 install_nvim_plug() {
   printf "Installing nvim plug...\n"
-  curl -fLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
+  curl -fLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
 add_dotfiles() {
   printf "Adding dotfiles...\n"
-  cp -rT skel "$HOME"
-  cp -rT skel-manjaro "$HOME"
-  mkdir -p "$HOME/src/theodus"
+  cp -rT skel "${HOME}"
+  cp -rT skel-manjaro "${HOME}"
+  mkdir -p "${HOME}/src/theodus"
 }
 
 set_compilers() {
   printf "Setting cc, c++, ld ...\n"
-  sudo ln -s -f /usr/bin/clang /usr/bin/cc
-  sudo ln -s -f /usr/bin/clang++ /usr/bin/c++
-  sudo ln -s -f /usr/bin/ld.lld /usr/bin/ld
+  sh "${HOME}/bin/toolchain-symlinks.sh"
 }
 
 install_fonts() {
@@ -45,12 +43,18 @@ install_fonts() {
   fc-cache
 }
 
-require clang clang-format git go lld lldb nvim rg tmux
+install_tools() {
+  printf "Installing rustup...\n"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+require clang clang-format git lld lldb nvim rg tmux
 install_tmux_tpm
 install_nvim_plug
 add_dotfiles
 set_compilers
 install_fonts
+install_tools
 
-echo "nvim: run :PlugInstall"
-echo "tmux: run prefix + I"
+echo "nvim: run `:PlugInstall`"
+echo "tmux: run `prefix + I`"
--- skel/.config/bspwm/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-$HOME/.config/polybar/launch.sh &
-feh --bg-scale $HOME/.config/bspwm/wallpaper.jpg &
-
