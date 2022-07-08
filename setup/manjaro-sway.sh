8294be38a25056e3c27439a91e864d2904c5f4ab
Theo Butler <theodusbutler@gmail.com>
2022-07-08 19:33:26 -0400

    add initial sway config

--- setup/manjaro-sway.sh
+++ setup/manjaro-sway.sh
@@ -7,7 +7,7 @@ sudo sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf
 sudo pacman -Syu && sudo pacman -Syyu
 pamac install --no-confirm \
   base-devel clang cmake lld llvm llvm-libs \
-  git neovim ripgrep shellcheck \
+  git neovim nvm ripgrep shellcheck \
   nerd-fonts-jetbrains-mono ttf-juliamono \
   gammastep \
   brave-browser visual-studio-code-bin
@@ -17,4 +17,3 @@ cp -rT configs "${HOME}"
 
 printf "[_] Installing rustup\n"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
