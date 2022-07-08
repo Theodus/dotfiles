7d33b673885ef51455f7b87cb487859ab7418a4a
Theo Butler <theodusbutler@gmail.com>
2022-07-08 10:48:28 -0400

    restructure

---
+++ setup/manjaro-sway.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. ./setup/common.sh
+
+printf "[_] Install packages\n"
+sudo sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf
+sudo pacman -Syu && sudo pacman -Syyu
+pamac install --no-confirm \
+  base-devel clang cmake lld llvm llvm-libs \
+  git neovim ripgrep shellcheck \
+  nerd-fonts-jetbrains-mono ttf-juliamono \
+  gammastep \
+  brave-browser visual-studio-code-bin
+
+printf "[_] Copy configs\n"
+cp -rT configs "${HOME}"
+
+printf "[_] Installing rustup\n"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
