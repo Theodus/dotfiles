0f040027a68332fcead07821dfc3ad9ed44d6a04
Theo Butler <theodusbutler@gmail.com>
2024-10-19 12:20:26 -0400

    nvim: options

---
+++ configs/.config/nvim/lua/options.lua
@@ -0,0 +1,29 @@
+-- columns
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = 'yes'
+-- indent
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.shiftwidth = 4 
+vim.o.softtabstop = 4
+vim.o.tabstop = 4
+-- search
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.smartcase = true
+-- etc
+vim.o.backup = false
+vim.o.clipboard = 'unnamedplus'
+vim.o.colorcolumn = '80'
+vim.o.laststatus = 2
+vim.o.scrolloff = 8
+vim.o.showmode = true
+vim.o.swapfile = false
+vim.o.termguicolors = true
+vim.o.timeoutlen= 400
+vim.o.undofile = true
+vim.o.updatetime = 50
+vim.o.wrap = false
+vim.g.mapleader = ' '
